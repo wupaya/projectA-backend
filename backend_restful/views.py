b41abf5e63dab0216effe6de9a70c5966dffd2da
proton <proton.roy@gmail.com>
2019-02-02 16:38:07 +0600

    sub er view define koreci

--- backend_restful/views.py
+++ backend_restful/views.py
@@ -75,7 +75,7 @@ class ValidateParamView(APIView):
           return Response(HelloMessageSerializer(helloparamobject).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 		
-class SignUpView(APIView):
+class SignView(APIView):
     '''
     **Title**
     ----
@@ -142,3 +142,11 @@ class SignUpView(APIView):
             sign_up_input_serializer.save()
             pass
         return Response(sign_up_input_serializer.errors, status.HTTP_400_BAD_REQUEST)
+class SubView(APIView):
+       
+	def get(self,request):
+	    serializer = SubSerializer(data=request.data)
+		return Response(serializer.data, status.HTTP_200_OK)
+	def post(self, request):
+	    Serializer = SubSerializer(data=request.data)
+		return Response(serializer.data, status.HTTP_200_OK)
