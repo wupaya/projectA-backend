617a4faa22cc009737ceb78f10efb401ff85ce9e
iamnoyon <mdmahfujarrahmanoyon09@gmail.com>
2019-02-20 23:34:16 +0600

    View Er Bug FIx Koresi

--- noyon_view.py
+++ noyon_view.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .noyon import Noyon
+from .noyon_serializer import NoyonSerzlier
 
 class NoyonView(APIView):
     def get(self,request):
