f1a2d311f1adc8db649d2afb42587ab885539b7b
iamnoyon <mdmahfujarrahmanoyon09@gmail.com>
2019-02-24 21:55:58 +0600

    URL separete korsi

--- backend_restful/urls.py
+++ backend_restful/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
 from .views import SignUpView, SubView
 
-from .noyon_view import NoyonView
 #use router if you don't want to make url using regular expression like urlpatterns
 router = routers.DefaultRouter(trailing_slash = False)
 
@@ -11,6 +10,6 @@ router = routers.DefaultRouter(trailing_slash = False)
 urlpatterns = [
     url(r'^signup/$', SignUpView.as_view(), name='signup'), #just using response method
      url(r'^sub/$', SubView.as_view(), name='sub'), #just using response method
-     url(r'^noyon/$', NoyonView.as_view(), name='noyon'), #just using response method
+     url(r'^', include('backend_restful.noyon_urls')), #just using response method
      
 ]
