8e4d64f91521efcb1916d84d81f5adb56d009918
iamnoyon <mdmahfujarrahmanoyon09@gmail.com>
2019-02-26 23:40:39 +0600

    onekgula test automation koresi

--- backend_restful/test_noyon.py
+++ backend_restful/test_noyon.py
@@ -24,3 +24,23 @@ class NoyonTest(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, data)
+        
+    def test_noyon3(self):
+        url = reverse('noyon3')
+        data = {
+            'greetings' : "Hi There, i'm Noyon"
+            
+        }
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
+        
+    def test_noyon4(self):
+        url = reverse('noyon4')
+        data = {
+            'greetings' : "Hey, i'm Noyon, What's up?"
+            
+        }
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
