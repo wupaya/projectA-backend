cc641dfbca65b30963a66aaaf835594bc726a0f9
mhsn06 <mhsn06@gmail.com>
2019-03-02 23:25:09 +0600

    amr url define koresi

---
+++ backend_restful/hassan_urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from .noyon_view import NoyonView
+from .noyon_view import Noyon2View
+from .noyon_view import Noyon3View
+from .noyon_view import Noyon4View
+from .noyon_view import Noyon5View
+
+urlpatterns = [
+    url(r'^noyon/$', NoyonView.as_view(), name='noyon'),
+    url(r'^noyon2/$', Noyon2View.as_view(), name='noyon2'),
+    url(r'^noyon3/$', Noyon3View.as_view(), name='noyon3'),
+    url(r'^noyon4/$', Noyon4View.as_view(), name='noyon4'),
+    url(r'^noyon5/$', Noyon5View.as_view(), name='noyon5'),
+]
