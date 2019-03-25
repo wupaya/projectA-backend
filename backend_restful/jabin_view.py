4281339d556d84f48e9454425f367b9b0a200db1
JabinJoty <jabinjoty@gmail.com>
2019-03-25 23:14:14 +0600

    missing serializer error thik korsi

--- backend_restful/jabin_view.py
+++ backend_restful/jabin_view.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .jabin import Jabin,jabin2
-from .jabin_serializer import JabinSerializer
+from .jabin_serializer import JabinSerializer,jabinSerializer2
 
 class JabinView(APIView):
     def get(self,request):
