ba1f0192b016eec8ad5a0a162a21010d62b698ac
JabinJoty <jabinjoty@gmail.com>
2019-03-31 00:45:28 +0600

    sob error fix koresi

--- backend_restful/jabin_view.py
+++ backend_restful/jabin_view.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .jabin import Jabin,jabin2
-from .jabin_serializer import JabinSerializer,jabinSerializer2
+from .jabin import Jabin,Jabin2
+from .jabin_serializer import JabinSerializer,JabinSerializer2
 
 class JabinView(APIView):
     def get(self,request):
@@ -11,8 +11,7 @@ class JabinView(APIView):
     def post(self,request):
         serializer = JabinSerializer(Jabin())
         return Response (serializer.data,status.HTTP_200_OK)
-         
- class JabinView2(APIView):
+class JabinView2(APIView):
     def get(self,request):
         serializer = JabinSerializer2(Jabin2())
         return Response (serializer.data,status.HTTP_200_OK)
