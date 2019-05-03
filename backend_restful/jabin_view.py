e0fc3664b05c8ad5b2904096908b693568e61fc7
JabinJoty <jabinjoty@gmail.com>
2019-05-03 22:45:46 +0600

    view add koresi

--- backend_restful/jabin_view.py
+++ backend_restful/jabin_view.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .jabin import Jabin,Jabin2,Jabin3
-from .jabin_serializer import JabinSerializer,JabinSerializer2,JabinSerializer3
+from .jabin import Jabin,Jabin2,Jabin3,Jabin3
+from .jabin_serializer import JabinSerializer,JabinSerializer2,JabinSerializer3,JabinSerializer3
 
 class JabinView(APIView):
     def get(self,request):
@@ -17,6 +17,13 @@ class JabinView2(APIView):
         return Response (serializer.data,status.HTTP_200_OK)
     def post(self,request):
         serializer = JabinSerializer2(Jabin2())
+        return Response (serializer.data,status.HTTP_200_OK)
+class JabinView3(APIView):
+    def get(self,request):
+        serializer = JabinSerializer3(Jabin3())
+        return Response (serializer.data,status.HTTP_200_OK)
+    def post(self,request):
+        serializer = JabinSerializer3(Jabin3())
         return Response (serializer.data,status.HTTP_200_OK)
 class JabinView3(APIView):
     def get(self,request):
