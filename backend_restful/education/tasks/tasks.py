7eb6d527650249db22624df87c6ad6e483b5d4ce
Hassan <mhsn06@gmail.com>
2019-11-29 17:17:52 +0600

    implemented tags and tasks

--- backend_restful/education/tasks/tasks.py
+++ backend_restful/education/tasks/tasks.py
@@ -11,49 +11,31 @@ class tasks:
     response = {}
     def __init__(self, data={}):
         data["associated"] = "5ddd62ffd8639286d599dcd6"
-        data["designations"] = "5ddd62ffd8639286d599dcd7"
+        data["designation"] = "5ddd62ffd8639286d599dcd7"
         data["tag"] = "5ddd62ffd8639286d599dcd8"
         #query database for dashboard info
         client = pymongo.MongoClient(mongodb_url)
         db = client.test
         education = db.education
         user_id = data.get("user_info")
-        #pprint(user_id)
+        
         query_result = education.find_one(
-              {"_id" : ObjectId(user_id),
-              "associated._id": ObjectId(data.get("associated")),
-              "associated.designations._id": ObjectId(data.get("designations")),
-              "associated.designations.tags._id" : ObjectId(data.get("tag"))
-              },
-              {"associated.designations.tags._id":1,
-               "associated.designations.tags.$":1
-               }
-        )
-        #this is to avoid ObjectId not serializer error
-        #query_result["_id"] = user_id
+              {
+              "_id" : ObjectId(user_id)
+              }
+            )
+        #filter results here
+
         print("showing results")
         pprint(query_result)
-        # query_result = { 
-        #     "_id":ObjectId(user_id),
-        #     "associated": [
-        #     {"id": 1, "short_name":"BRUR", "long_name":"Begum Rokeya University, Rangpur", "designation":"Parent",
-        #     "status": "joined",
-        #     "join_date": "July 1st 2019",
-        #     "allowed_services": [
-        #     { "id": 1, "designation": "Parent" },
-        #     { "id": 2, "designation": "Teacher" } ]},
-        #     {"id": 2, "short_name":"RGC", "long_name":"Rangpur Government College, Rangpur", "designation":"Parent",
-        #     "status": "joined",
-        #     "join_date": "July 1st 2019",
-        #     "allowed_services": [
-        #     { "id": 1, "designation": "Parent" },
-        #     { "id": 2, "designation": "Teacher" } ]},
-        #     ]
-        # }
 
-        #query database for associates
-        #{"_id":ObjectId(user_id)}
-        #post_id = education.insert_one(query_result).inserted_id
-        
-        #print(json.dumps(query_result, cls=JSONEncoder))
-        self.response=json.loads(json.dumps(query_result, default=str))
+        for associate in query_result["associated"]:
+            if data["associated"] == str(associate.get("_id")):
+                for designation in associate["designations"]:
+                    if data["designation"] == str(designation.get("_id")):
+                        for tag in designation["tags"]:
+                            if data["tag"] == str(tag.get("_id")):
+                                self.response=json.loads(json.dumps(tag, default=str))
+                                return
+
+        self.response=json.loads(json.dumps({"error": "retriving_tast_list"}, default=str))
