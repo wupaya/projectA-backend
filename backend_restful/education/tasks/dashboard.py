fe4593fced7a98f29394deedb01b037182ffde3d
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-12-02 17:48:34 +0600

    Removed the comment code from dashboard.py in education/tasks directory

--- backend_restful/education/tasks/dashboard.py
+++ backend_restful/education/tasks/dashboard.py
@@ -20,31 +20,5 @@ class dashboard:
               {"_id" : ObjectId(user_id)},
               {"_id":0, "associated.designations.tags":0 }
         )
-        #this is to avoid ObjectId not serializer error
-        #query_result["_id"] = user_id
-        #print("showing results")
-        #pprint(query_result)
-        # query_result = { 
-        #     "_id":ObjectId(user_id),
-        #     "associated": [
-        #     {"id": 1, "short_name":"BRUR", "long_name":"Begum Rokeya University, Rangpur", "designation":"Parent",
-        #     "status": "joined",
-        #     "join_date": "July 1st 2019",
-        #     "allowed_services": [
-        #     { "id": 1, "designation": "Parent" },
-        #     { "id": 2, "designation": "Teacher" } ]},
-        #     {"id": 2, "short_name":"RGC", "long_name":"Rangpur Government College, Rangpur", "designation":"Parent",
-        #     "status": "joined",
-        #     "join_date": "July 1st 2019",
-        #     "allowed_services": [
-        #     { "id": 1, "designation": "Parent" },
-        #     { "id": 2, "designation": "Teacher" } ]},
-        #     ]
-        # }
-
-        #query database for associates
-        #{"_id":ObjectId(user_id)}
-        #post_id = education.insert_one(query_result).inserted_id
-        
         #print(json.dumps(query_result, cls=JSONEncoder))
         self.response=json.loads(json.dumps(query_result, default=str))
