3c444ac92805993939faee83eeded85702f1e546
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-12-06 23:38:26 +0600

    join institute modified 3

--- backend_restful/education/tasks/join_institute.py
+++ backend_restful/education/tasks/join_institute.py
@@ -14,7 +14,7 @@ class join_institute:
 
         if serializer.is_valid():
             public_page_id = serializer.validated_data.get("institute_id")
-            designations = serializer.validated_data.get("designations")
+            incoming_designations = serializer.validated_data.get("designations")
 
             #query database for dashboard info
             #str(users.find_one({"$and":[{"email": "mhsn06@gmail.com"},{"password": "1234"}]}))
@@ -30,38 +30,24 @@ class join_institute:
             #return not found error
             return Response({"status_code":"page_not_found", "default_description":"no such thing exits in the system"}, status=status.HTTP_200_OK)
 
+            stored_designations = [ppageid.get("designation")]
+
+            if(lenght<=0):
+                return Response({"status_code":"designation_error", "default_description":"no matched designation found"}, status=status.HTTP_200_OK)
+            
+            matched_designations = []
+
+            foreach designations in incoming_designations:
+                foreach designation in stored_designations:
+
+                    if(designations==designation):
+                        matched_designations.append(designation)
             #this is to avoid ObjectId not serializer error
             #query_result["_id"] = user_id
             query_result = { 
                 "_id":ObjectId(user_id),
                 "associated": [
-                {"_id": ObjectId(), "long_name": ppageid.get("page_title"), "page_type": ppageid.get("type_of_institute"), "description": ppageid.get("description"), "designations":[
-                    {"_id": ObjectId(), "title":"Parent", "tags":[
-                        {"_id":ObjectId(), "title":"", "tasks":[
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                        ]},
-                        {"_id":ObjectId(), "title":"", "tasks":[
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                        ]},
-                    ]},
-                    {"_id": ObjectId(), "title":"Teacher", "tags":[
-                        {"_id":ObjectId(), "title":"", "tasks":[
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                        ]},
-                        {"_id":ObjectId(), "title":"", "tasks":[
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                            {"_id":ObjectId(), "title":""},
-                        ]},
-                    ]}
-                ]}
-                ]
+                {"_id": ObjectId(), "long_name": ppageid.get("page_title"), "page_type": ppageid.get("type_of_institute"), "description": ppageid.get("description"), "designations":[matched_designations]
             }
             #pprint("user id "+ user_id)
             res = education.update_one({"_id":ObjectId(user_id)}, {"$set": {"associated":query_result.get("associated", [])}}, upsert=True)
