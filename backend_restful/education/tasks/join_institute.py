59529941449f07d684b5c02bbdc5067c56bbb516
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-12-07 23:09:09 +0600

    join institute modified

--- backend_restful/education/tasks/join_institute.py
+++ backend_restful/education/tasks/join_institute.py
@@ -44,7 +44,7 @@ class join_institute:
             query_result = { 
                 "_id":ObjectId(user_id),
                 "associated": [
-                {"_id": ObjectId(), "long_name": ppageid.get("page_title"), "page_type": ppageid.get("type_of_institute"), "description": ppageid.get("description"), "designations":[matched_designations]
+                {"_id": ObjectId(), "long_name": ppageid.get("page_title"), "page_type": ppageid.get("type_of_institute"), "description": ppageid.get("description"), "designations": matched_designations
             }
             #pprint("user id "+ user_id)
             res = education.update_one({"_id":ObjectId(user_id)}, {"$set": {"associated":query_result.get("associated", [])}}, upsert=True)
