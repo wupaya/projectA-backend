bed647a7e655020c1c612fcfda4f252e458d8a4d
Hassan <mhsn06@gmail.com>
2019-12-17 10:59:56 +0600

    add default tags and tasks to public page creation

--- backend_restful/education/tasks/create_public_page.py
+++ backend_restful/education/tasks/create_public_page.py
@@ -1,30 +1,110 @@
-from backend_restful.db import users
 from backend_restful.DBHandler import DBHandler
 from bson.objectid import ObjectId
 from pprint import pprint
 from rest_framework import serializers
 
+
 class create_public_page:
     response = {}
+
     def __init__(self, data={}):
 
         serializer = PublicPageSerializer(data=data["data"])
 
         if serializer.is_valid():
-            #query database 
+            # query database
             client = DBHandler.get_database_client()
             db = client.test
             public_pages = db.public_pages
-            #user_id = data.get("user_info")
-            found_page = public_pages.find_one({"page_title": serializer.validated_data.get("page_title")})
-            if(found_page is None):
-                #serializer.validated_data["_id"] = "brur"
+            # user_id = data.get("user_info")
+
+            
+            found_page = public_pages.find_one(
+                {"page_title": serializer.validated_data.get("page_title")}
+            )
+            if found_page is None:
+                #assign it to "designation" key
+                default_designations = [
+                        {
+                            "_id": ObjectId(),
+                            "title": "page_admin",
+                            "tags": [
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "approval_requests",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Approve Join Request", "task_nice_id":"approve_join_request"}
+                                    ],
+                                },
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "manage_people",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Approve Join Request", "task_nice_id":"add_people"},
+                                        {"_id": ObjectId(), "title": "Modify Responsibility", "task_nice_id":"modify_responsibility"},
+                                        {"_id": ObjectId(), "title": "Suspend user association", "task_nice_id":"suspend_association"}
+                                    ],
+                                },
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "manage_class",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Suspend user association", "task_nice_id":"take_attendance"}
+                                    ],
+                                },
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "analysis",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Student attendance analysis", "task_nice_id":"student_attendance_analysis"}
+                                    ],
+                                }
+                            ],
+                        },
+                        {
+                            "_id": ObjectId(),
+                            "title": "Teacher",
+                            "tags": [
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "manage_class",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Suspend user association", "task_nice_id":"take_attendance"}
+                                    ],
+                                }
+                            ],
+                        },
+                        {
+                            "_id": ObjectId(),
+                            "title": "Guardian",
+                            "tags": [
+                                {
+                                    "_id": ObjectId(),
+                                    "title": "manage_attendance",
+                                    "tasks": [
+                                        {"_id": ObjectId(), "title": "Child Attendance Analysis", "task_nice_id":"attendance_analysis"}
+                                    ],
+                                }
+                            ],
+                        }
+                    ]
+            
+                serializer.validated_data["designation"] = default_designations
                 post_id = public_pages.insert_one(serializer.validated_data).inserted_id
-                self.response = {"status_code":"page_creation_successfull", "default_description":"successfully created page", "id": str(post_id)}
+                self.response = {
+                    "status_code": "page_creation_successfull",
+                    "default_description": "successfully created page",
+                    "id": str(post_id),
+                }
             else:
-                self.response = {"status_code":"registration_failed", "default_description":"already exist", "id": str(found_page["_id"])}
+                self.response = {
+                    "status_code": "registration_failed",
+                    "default_description": "already exist",
+                    "id": str(found_page["_id"]),
+                }
         else:
-            self.response ={"error": serializer.errors}
+            self.response = {"error": serializer.errors}
+
 
 class PublicPageSerializer(serializers.Serializer):
     page_title = serializers.CharField()
