1ad6feda2103dbd81319620c203c812fc4fbbc56
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-12-29 22:39:07 +0600

    got success message and trying toh efficient

--- backend_restful/education/tasks/invite_people.py
+++ backend_restful/education/tasks/invite_people.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from datetime import datetime, timedelta
 from bson.objectid import ObjectId
 from random import randint
+from pprint import pprint
 
 class InvitePeopleSerializer(serializers.Serializer):
     phone_no = serializers.CharField()
@@ -31,27 +32,31 @@ class invite_people:
                 # todo store user phon
                 new_user_id = users.insert_one(
                     {"phone_no": users_incoming_phone_no}).inserted_id
+            else:
+                new_user_id = serched_phone_number.get("_id")
 
-                invitation_document_object = {
-                    "_id": ObjectId(),
-                    "phone_no": serializer.validated_data.get("phone_no"),
-                    "expired_date": datetime.now()+timedelta(hours=48),
-                    "invitation_from": user_id,
-                    "institute_id": serializer.validated_data.get("institute_id"),
-                    "designations": serializer.validated_data.get("designations"),
-                    "verification_code": randint(1000, 9999)  
-                }
-                print(new_user_id)
-                res = education.update_one({"_id": ObjectId(new_user_id)}, {"$push":
-                {"invitations":invitation_document_object}}, upsert=True)
+            invitation_document_object = {
+                "_id": ObjectId(),
+                "phone_no": serializer.validated_data.get("phone_no"),
+                "expired_date": datetime.now()+timedelta(hours=48),
+                "invitation_from": user_id,
+                "institute_id": serializer.validated_data.get("institute_id"),
+                "designations": serializer.validated_data.get("designations"),
+                "verification_code": randint(1000, 9999)  
+            }
+            #print(new_user_id)
+            res = education.update_one({"_id": ObjectId(new_user_id)}, {"$push":
+            {"invitations":invitation_document_object}}, upsert=True)
 
-                invitation_id = education.find_one({"_id": ObjectId(invitation_document_object)})
+            invitation_id = education.find_one(invitation_document_object.get("_id"))
 
-                #sms queue collection
-                sms_queue = db.sms_queue
-                sms_id = sms_queue.insert_one({"_id":ObjectId(), "invitation_id": invitation_id, "send_status": "pending"}).inserted_id
-
-                self.response = {"s":"success"}
+            pprint(invitation_document_object)
+            #sms queue collection
+            sms_queue = db.sms_queue
+            sms_id = sms_queue.insert_one({"_id":ObjectId(), "invitation_id": invitation_id, "send_status": "pending"}).inserted_id
+            
+            if res.matched_count>0:
+                self.response={"s":"success"}
             else:
                 self.response = {"message": "something went wrong"}
         else:
