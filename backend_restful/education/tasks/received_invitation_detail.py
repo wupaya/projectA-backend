95b9d8ea4441c8832cc33618d8b3c5f7d6b259d8
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2019-12-31 00:20:56 +0600

    got invitation details

--- backend_restful/education/tasks/received_invitation_detail.py
+++ backend_restful/education/tasks/received_invitation_detail.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from backend_restful.DBHandler import DBHandler
 from bson.objectid import ObjectId
+import json
 from pprint import pprint
 class IncomingDataSerializer(serializers.Serializer):
     invitation_id = serializers.CharField()
@@ -22,9 +23,12 @@ class received_invitation_detail:
             invitation_list = searched_user_id.get("invitations")
 
             for stored_invitation in invitation_list:
-                if(stored_invitation["_id"] == serializer.validated_data.get("invitation_id")):
-                    self.response = stored_invitation
+                #print(str(stored_invitation["_id"]))
+                #print(stored_invitation["_id"])
+                if(str(stored_invitation["_id"]) == serializer.validated_data.get("invitation_id")):
+                    self.response = json.loads(json.dumps(stored_invitation, default=str))
+                    return
                 else:
-                    self.response = {"message": "something went wrong."}
+                    self.response = json.loads(json.dumps({"message": "something went wrong."}, default=str))
         else:
             self.response = serializer.errors
